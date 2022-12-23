aadb143a025d39a0b237741be3075d1ed7132392
Vytman <vytman@gmail.com>
2022-12-23 16:42:55 +0000

    Setup app for deployment

--- public/js/app.js
+++ public/js/app.js
@@ -6,7 +6,7 @@ const messageTwo = document.getElementById('message-2');
 weatherForm.addEventListener('submit', e => {
 	e.preventDefault();
 
-	fetch('http://localhost:3000/weather?address=' + search.value).then(response => {
+	fetch('/weather?address=' + search.value).then(response => {
 		messageOne.textContent = 'Loading...';
 		messageTwo.textContent = '';
 		response.json().then(data => {
